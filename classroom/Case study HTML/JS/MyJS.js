6f213fad0d10afd8f78a35f3ce40a850415d735e
cookedm87 <34936174+cookedm87@users.noreply.github.com>
2018-01-10 15:01:15 +0000

    Add files via upload

--- classroom/Case study HTML/JS/MyJS.js
+++ classroom/Case study HTML/JS/MyJS.js
@@ -1,5 +1,42 @@
-/*When used clicks button toggle hiding and showing drop content*/
+/*-----------------------------------------------------------------------------------
 
-function navButton() {
+FICTIONAL WEBSITE SCRIPT FILE
+
+-----------------------------------------------------------------------------------*/
+
+/*MENU DEVICE SCRIPT*/
+
+/*When user clicks button toggle hiding and showing drop content*/
+
+$("#mainNav").click(function(){
+                        $("#mainMenu").slideToggle();
+                    });
+
+$("#mainMenu li").on("click", function(){
+                        if($(window).width() < 1024){
+                            $("#mainNav").click();
+                        }
+                    });
+
+$(window).resize(function(){
+    if($(window).width() < 1024) {
+        $("#mainNav").removeAttr("style");
+    }
+});
+
+/*-----------------------SMOOTH SCROLLING-------------------*/
+
+$(function(){
+   $('.navC a, .actionB a, .logoC a').bind('click', function(event){
+      var $anchor = $(this);
+       $('html, body').stop().animate({
+           scrollTop: 
+           $($anchor.attr('href')).offset().top - 100
+       }, 1000);
+       event.preventDefault();
+   }); 
+});
+
+/*function navButton() {
 	document.getElementById("mainMenu").classList.toggle("show");
-}
+}*/
